9abdd286c5c0cb91a090b38b35c274b6052f685e
hantmac <hantmac@outlook.com>
2020-05-19 16:08:46 +0800

    update

--- fuckdb.rb
+++ fuckdb.rb
@@ -3,8 +3,8 @@
 class Fuckdb < Formula
   desc "From database generate go struct,help you fuck db fileds.\n User fuckdb generate to generate fuckdb.json and config it then fuckdb go"
   homepage "https:github.com/hantmac/fuckdb"
-  url "https://github.com/hantmac/fuckdb/releases/download/v0.3/fuckdb_macos.tar.gz"
   version "0.3"
+  url "https://github.com/hantmac/fuckdb/releases/download/v0.3/fuckdb_macos.tar.gz"
   sha256 "fc9bd406bb38991fae669794b55ad617902728569a4fc883a1ebb22c5297c1df"
 # depends_on "cmake" => :build
 
